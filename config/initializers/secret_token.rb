e20b272f5ce5c85b6e776f3686aa14cc24326ac9
Fafa <fafa@Fafa.(none)>
2012-07-27 23:50:47 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PediatricApp::Application.config.secret_token = '32d0a8c31ca3b4e1b9dda995d1555ee989bb5c3c84563fba5d5beaa4017e0474915200182d98376ea1d665bf5a186fa5490415986631f400a53d494975920871'
